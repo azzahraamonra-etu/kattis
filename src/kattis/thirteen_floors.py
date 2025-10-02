86948a962ef09bcb816589bf52d06352264d4f0e
azzahramona <azzahramona@gmail.com>
2025-10-02 22:13:39 +0200

    fix(thirteen_floors): update function

--- src/kattis/thirteen_floors.py
+++ src/kattis/thirteen_floors.py
@@ -1,15 +1,16 @@
+"""Module for solving the 13floors problem from Kattis."""
+
 def thirteen_floors(f1: int) -> int:
     """
     Solves the 13floors problem from Kattis.
     URL: https://open.kattis.com/problems/13floors
 
     Args:
-    f1 (int): The floor number before adjustment.
+        f1 (int): The floor number before adjustment.
 
     Returns:
-    int: The adjusted floor number considering skipping floor 13.
+        int: The adjusted floor number considering skipping floor 13.
     """
     if f1 > 12:
         return f1 + 1
-    else:
-        return f1
+    return f1
